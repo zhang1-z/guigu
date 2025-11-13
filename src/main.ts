97d03e39dff9d8c927657b996c43d49b9be260ca
zhangwenrui <zwr@123.com>
2025-11-13 21:05:13 +0800

    登录路由静态搭建和模板封装登录业务

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,24 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './styles/index.scss'
 //引入路由
 import router from './router'
+
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//引入仓库
+import pinia from './store'
 //获取应用实例对象
 const app = createApp(App)
 //安装element-plus插件
 app.use(ElementPlus, {
   locale: zhCn,
 })
+
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//   app.component(key, component)
+// }
+
+
 //注册路由
 app.use(router)
+// 注册仓库
+app.use(pinia)
 app.mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,88 @@
 <template>
-  <div>
-    <h1>我是一级路由</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form"> 
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button :loading="loading" class="login_btn" type="primary" size="large" @click="login">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive, ref } from 'vue'
+import { useRouter} from 'vue-router'
+import { ElNotification } from 'element-plus'
+//引入用户相关的小仓库
+import useUserStore from '../../store/modules/user'
+let useStore = useUserStore()
+let route = useRouter()
+let loading = ref(false)
+//收集账号和密码的数据
+let loginForm = reactive({username:'admin', password:'111111'})
+const login =  async() => {
+  loading.value = true
+  //点击登录按钮以后干什么
+  //通知仓库发登录请求
+  //请求成功->首页展示数据的地方
+  //请求失败->弹出登录失败信息
+  try {
+    await useStore.userLogin(loginForm)
+    route.push('/')
+    ElNotification({
+      type: 'success',
+      message: '登录成功'
+    })
+    loading.value = false
+  } catch (error) {
+    loading.value = false
+    ElNotification({
+      type: 'error',
+      message: ( error as Error ).message
+    })
+  }
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('../../assets/images/background.jpg') no-repeat;
+  background-size: cover;
+  .login_form {
+  position: relative;
+  width: 80%;
+  top: 30vh;
+  background: url(../../assets/images/login_form.png) no-repeat;
+  background-size: cover;
+  padding: 40px;
+ }
+ h1 {
+  color: white;
+  font-size: 40px;
+ }
+ h2 {
+  font-size: 20px;
+  color: white;
+  margin: 20px 0;
+ }
+ .login_btn {
+  width: 100%;
+ }
+}
 
 </style>
